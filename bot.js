c0fa13e14ec596315f79ff188d4fdec1ef6c852a
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-05 10:46:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
+client.on('ready', () => {
     console.log("I am ready!");
 });
 
-client.on("message", (message) => {
+client.on('message', (message) => {
     if (message.content.startsWith("ping")) {
         message.channel.send("pong!");
     }
 });
+
+bot.on('guildMemberAdd', member =>) {
+    console.log('User' + member.user.username + 'has joined the server!')
+
+    var role = member.guild.roles.get('387029449574580234');
+    member.addRole(role)
+}
 
 client.login(process.env.BOT_TOKEN);
