3f523d0ed1e90627d396f76a6e034f5a3145efd5
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-05 11:07:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,30 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+var Discord = require("discord.js");
+var client = new Discord.Client();
+
+client.on('message', (message) => {
+
+    //Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content.toUpperCase(); //Takes the message, and makes it all uppercase
+    var prefix = '!Kn' //The text before commands, you can set this to whatever you want
+
+    //Ping/Pong Command
+    if (message.content.startsWith("ping")) {
+        message.channel.send("pong!");
+    }
+
+    //Deleting Specific Messages
+    if (message.channel.id === 387004194554642442) { //Checks if the message is in the specific channel
+        if (message.content !== '?') { //Checks if the message is not a question, if it's not, the following code will run
+            message.delete() //This deletes the message
+            message.channel.send(author, 'Please put a question mark in your message!')
+        }
+    }
+});
 
 client.on('ready', () => {
     console.log("I am ready!");
 });
 
-client.on('message', (message) => {
-    if (message.content.startsWith("ping")) {
-        message.channel.send("pong!");
-    }
-});
-
-client.on('guildMemberAdd', member =>) {
-    console.log('User' + member.user.username + 'has joined the server!')
-
-    var role = member.guild.roles.find('name', 'Meme Peasants');
-    member.addRole(role)
-}
-
-bot.on('disconnect', function (msg, code) {
-    if (code === 0) return console.error(msg);
-    bot.connect();
-});
 
 client.login(process.env.BOT_TOKEN);
