1965647d8647baecf5cdb86be74d4e0ae8c2c412
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-04 20:46:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let myRole = message.guild.roles.find("name", "Meme King");
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -14,7 +12,9 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if ((!message.content("?")) &&(!message.member.roles.has(myRole))) {
+    let myRole = message.guild.roles.find("name", "Meme King");
+
+    if ((!message.content("?")) &&(message.member.roles.has(myRole))) {
         message.delete(1000); //Supposed to delete message
     }
 });
