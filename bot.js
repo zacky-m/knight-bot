98e626fdf93181c70393e1f2d741f0f6e035eebf
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-06 00:38:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on('message', (message) => {
 
     //Deleting Specific Messages
     if (message.channel.id === '387004194554642442') { //Checks if the message is in the specific channel
-        if (message.content.includes('?') && (message.isMentioned('386959013809487873') || message.isMentioned('386960113539481610'))) { //Checks if the message is not a question, if it's not, the following code will run
-            message.channel.send('Thank you for your question! You should receive an answer soon.')
+        if (message.content.includes('?')) { //Checks if the message is not a question, if it's not, the following code will run
+            message.channel.send('Thank you for your question! You should receive an answer soon.');
         }
         else if (!message.content.includes('?') && (!message.isMentioned('386959013809487873') || !message.isMentioned('386960113539481610'))) {
             message.delete()
@@ -26,9 +26,9 @@ client.on('message', (message) => {
             message.delete()
             message.author.send('Please put @Meme King or @Edge Lords in your message!')
         }
-        else if (!message.content.includes('?') && (message.isMentioned('386959013809487873') || message.isMentioned('386960113539481610'))) {
+        else if (!message.content.includes('?')) {
             message.delete()
-            message.author.send('Please put a question mark in your message!')//bull
+            message.author.send('Please put a question mark in your message!')
         }
     }
 });
