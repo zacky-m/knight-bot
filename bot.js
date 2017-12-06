c9a0bfd212c8f7b4e88f1e823cfad93306ccdb67
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-06 00:19:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,6 @@ client.on('message', (message) => {
     var sender = message.author; //The person who sent the message
     var msg = message.content.toUpperCase(); //Takes the message, and makes it all uppercase
     var prefix = '!Kn' //The text before commands, you can set this to whatever you want
-    let kRole = message.guild.roles.get("386959013809487873");
-    let eRole = message.guild.roles.get("386960113539481610");
 
     //Ping/Pong Command
     if (message.content.startsWith(prefix + "ping")) {
@@ -20,15 +18,15 @@ client.on('message', (message) => {
         if (message.content.includes('?') && (message.isMentioned(kRole) || message.isMentioned(eRole))) { //Checks if the message is not a question, if it's not, the following code will run
             message.channel.send('Thank you for your question! You should receive an answer soon.')
         }
-        if (!message.content.includes('?') && (!message.isMentioned(kRole) || !message.isMentioned(eRole))) {
+        if (!message.content.includes('?') && (!message.isMentioned('386959013809487873' || !message.isMentioned('386960113539481610'))) {
             message.delete()
             message.author.send('Please put @Meme King or @Edge Lords as well as a question mark in your message!')
         }
-        if (message.content.includes('?') && (!message.isMentioned(kRole) || !message.isMentioned(eRole))) {
+        if (message.content.includes('?') && (!message.isMentioned('386959013809487873') || !message.isMentioned('386960113539481610'))) {
             message.delete()
             message.author.send('Please put @Meme King or @Edge Lords in your message!')
         }
-        if (!message.content.includes('?') && (message.isMentioned(kRole) || message.isMentioned(eRole))) {
+        if (!message.content.includes('?') && (message.isMentioned('386959013809487873') || message.isMentioned('386960113539481610'))) {
             message.delete()
             message.author.send('Please put a question mark in your message!')//bull
         }
