88b31da1588d4660b8feddf51d40136197b56eaf
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-05 23:23:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ client.on('message', (message) => {
     var sender = message.author; //The person who sent the message
     var msg = message.content.toUpperCase(); //Takes the message, and makes it all uppercase
     var prefix = '!Kn' //The text before commands, you can set this to whatever you want
+    var kRole = message.guild.roles.get("386959013809487873");
+    var eRole = message.guild.roles.get("386960113539481610");
 
     //Ping/Pong Command
     if (message.content.startsWith(prefix + "ping")) {
@@ -15,7 +17,7 @@ client.on('message', (message) => {
 
     //Deleting Specific Messages
     if (message.channel.id === '387004194554642442') { //Checks if the message is in the specific channel
-        if (message.content.includes('?') && message.content.includes('@Meme King') && message.content.includes('@Edge Lords')) { //Checks if the message is not a question, if it's not, the following code will run
+        if (message.content.includes('?') && (message.isMentioned(kRole) || message.isMentioned(eRole))) { //Checks if the message is not a question, if it's not, the following code will run
             message.channel.send('Thank you for your question! You should receive an answer soon.')
         }
         else if ((message.member.roles.some(r => ['Meme King', 'Edge Lords', 'Knight Boi'].includes(r.name)))) {
