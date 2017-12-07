bb779ed9cd2e55dbfa665f1c1cff49c3d61f513d
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-06 19:48:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('message', (message) => {
 });
 
 client.on('guildMemberAdd', member => { //Event for when a user joins the server
-    var nRole = member.guild.roles.get('name', 'Meme Peasants'); //Finding the "Meme Peasants" role
+    var nRole = member.guild.roles.get('387029449574580234'); //Finding the "Meme Peasants" role
 
     console.log('User' + member.user.username + 'has joined the server') //Sends a message to the console that someone has joined the server
     member.addRole(nRole) //Adds the "Meme Peasants" role to the new user
