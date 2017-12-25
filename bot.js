4be0fa05a6eff352a02cbaa4fe59daa39e4d7614
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-25 18:46:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', (message) => {
 
     const embed = new Discord.RichEmbed()
         .setTitle('This is a test title!')
-        .setAuthor(`${member.user}`, 'https://i.imgur.com/lm8s41J.png')
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
         .setColor(0x00AE86)
         .setDescription('This is a test description!')
 
