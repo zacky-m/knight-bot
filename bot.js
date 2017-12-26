56abde0162820d4b99772be76bec6ee3063fd96a
Zacky Makeen <zacky.makeen@outlook.com>
2017-12-25 21:39:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,20 @@
 var Discord = require("discord.js");
 var client = new Discord.Client();
 
+client.pointsMonitor(client, message);
+
 client.pointsMonitor = (client, message) => {
-    if (message.channel.type === "dm") return;
+    if (message.channel.type !== 'text') return;
     const settings = client.settings.get(message.guild.id);
     if (message.content.startswith(settings.prefix)) return;
-
     const score = client.points.get(message.author.id) || { points: 0, level: 0 };
     score.points++
-    message.channel.send("Wowee mr krabs")
+    const score = client.points.get(messag.eauthor.id) || { points: 0, level: 0 };
+    if (score.level < curLevel) {
+        message.reply(`You have leveled up to level **${curLevel}**!`);
+        score.level = curLevel;
+    }
+    client.points.set(message.author.id, score);
 }
 
 client.on('message', (message) => {
