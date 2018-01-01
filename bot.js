036980b702837e30508f41c45ee213e58eac46a9
Zacky Makeen <zacky.makeen@outlook.com>
2018-01-01 11:49:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,14 +60,16 @@ client.on('message', (message) => {
     }
 
     if (message.channel.id === '387075592929017867') { //Checks if the message is in the test channel
+
         if (!message.member.roles.has(kRole.id)) {
 
+
         }
-        if (message.content.includes(prefix + 'level')) { //Checks if the message has the prefix + the word level
+
+        if (message.content.includes(prefix + 'karma')) { //Checks if the message has the prefix + the word level
             message.channel.send({ embed }); //Sends the embedded message
         }
     }
-
 });
 
 client.on('guildMemberAdd', member => { //Event for when a user joins the server
