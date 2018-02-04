d5c39c472be9d2bb34519e57204572165a28f6ac
Zacky Makeen <zacky.makeen@outlook.com>
2018-02-04 14:27:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', (message) => {
 
     if (message.channel.id === '386956245984215052') { //Checks if the message is in the memes channel
         if (message.member.roles.has(qRole.id)) { //Checks if the sender has the role
-            message.pin() //Pins the message
+            message.pin() //Pins the messages
         }
     }
 
